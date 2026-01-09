7a92063111799f3dc91c47780995cafb074c5d1a
r5 <dymaxionfive@gmail.com>
2026-01-09 16:39:05 +0100

    Revert "VSN-3297: Use GitHub application to authorize metrics publisher"
    This reverts commit 72454b617201ce1a1b560ca9c413a3c542670323.

--- src/main/kotlin/com/domanskii/Application.kt
+++ src/main/kotlin/com/domanskii/Application.kt
@@ -1,6 +1,5 @@
 package com.domanskii
 
-import com.domanskii.auth.GitHubAppAuth
 import com.domanskii.plugins.configureAuthentication
 import com.domanskii.plugins.configureRouting
 import com.domanskii.plugins.configureSerialization
@@ -13,8 +12,7 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import mu.KotlinLogging
-import java.nio.file.Files
-import kotlin.io.path.absolutePathString
+import org.kohsuke.github.GitHubBuilder
 
 private val log = KotlinLogging.logger {}
 
@@ -28,9 +26,7 @@ fun main() {
 fun Application.module() {
     val secretHeader = System.getenv("SECRET_HEADER")
     val ghRepo = System.getenv("GH_REPO")
-    val ghAppId = System.getenv("GH_APP_ID")
-    val ghInstallationId = System.getenv("GH_APP_INSTALLATION_ID")
-    val ghPrivateKey = System.getenv("GH_APP_PRIVATE_KEY")
+    val ghToken = System.getenv("GH_TOKEN")
     val ghDefaultBranch = System.getenv("GH_DEFAULT_BRANCH")
     val dbHost = System.getenv("DB_HOST")
     val dbName = System.getenv("DB_NAME")
@@ -39,22 +35,7 @@ fun Application.module() {
 
     DatabaseFactory.init(dbHost, dbName, dbUsername, dbPassword)
     val storage = StorageImpl()
-
-    val ghPrivateKeyPath = Files.createTempFile(
-        "gh_app_private_key",
-        if (ghPrivateKey.startsWith("-----BEGIN")) ".pem" else ".key"
-    ).apply {
-        Files.writeString(this, ghPrivateKey)
-    }.absolutePathString()
-    
-    // Create GitHub App authenticator and get authenticated client
-    val gitHubAuth = GitHubAppAuth(
-        appId = ghAppId,
-        privateKeyPath = ghPrivateKeyPath,
-        installationId = ghInstallationId
-    )
-    val gitHubClient = gitHubAuth.getAuthenticatedClient()
-    
+    val gitHubClient = GitHubBuilder().withOAuthToken(ghToken).build()
     val github = GitHub(ghRepo, ghDefaultBranch, gitHubClient)
     val markdownService = MarkdownService()
     val metricsService = MetricsService(storage, github, markdownService)
@@ -68,9 +49,7 @@ fun assertEnvVariables() {
     log.info { "Asserting env variables..." }
     require(!System.getenv("SECRET_HEADER").isNullOrBlank()) { "SECRET_HEADER environment variable is missing or blank" }
     require(!System.getenv("GH_REPO").isNullOrBlank()) { "GH_REPO environment variable is missing or blank" }
-    require(!System.getenv("GH_APP_ID").isNullOrBlank()) { "GH_APP_ID environment variable is missing or blank" }
-    require(!System.getenv("GH_APP_INSTALLATION_ID").isNullOrBlank()) { "GH_APP_INSTALLATION_ID environment variable is missing or blank" }
-    require(!System.getenv("GH_APP_PRIVATE_KEY").isNullOrBlank()) { "GH_APP_PRIVATE_KEY environment variable is missing or blank" }
+    require(!System.getenv("GH_TOKEN").isNullOrBlank()) { "GH_TOKEN environment variable is missing or blank" }
     require(!System.getenv("GH_DEFAULT_BRANCH").isNullOrBlank()) { "GH_DEFAULT_BRANCH environment variable is missing or blank" }
     require(!System.getenv("DB_HOST").isNullOrBlank()) { "DB_HOST environment variable is missing or blank" }
     require(!System.getenv("DB_NAME").isNullOrBlank()) { "DB_NAME environment variable is missing or blank" }
